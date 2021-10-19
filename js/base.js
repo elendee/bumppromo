13ccc86504610f8f3efa094f99af93bd4f6239e6
Kerry O'Connor <kerryoco@gmail.com>
2021-10-19 11:28:01 -0400

    menu

---
+++ js/base.js
@@ -0,0 +1,63 @@
+import env from './env.js'
+
+const links = document.querySelectorAll('a')
+
+const header = document.querySelector('#header')
+const nav_items = header.querySelectorAll('.nav-item')
+
+const locale = location.href.match(/localhost/) || location.href.match(/resource.oko/) ? 'dev' : 'live'
+
+
+const add_cases = item => {
+	
+	const use_cases = [{
+		title: 'golf',
+		href: 'golf',
+	},{
+		title: 'hair',
+		href: 'hair',
+	}]
+	const ul = document.createElement('ul')
+	for( const use of use_cases ){
+		ul.appendChild( build_use_link( use ) )
+	}
+	item.appendChild( ul )
+
+}
+
+
+const build_use_link = use => {
+
+	const item = document.createElement('div')
+	item.classList.add('use-case')
+	const link = document.createElement('a')
+	link.href = env.APP_ROOT + '/' + use.href
+	item.innerHTML = use.title
+	item.appendChild( link )
+	return item
+
+}
+
+
+
+
+
+// INIT
+
+
+// easier to add / remove use case links from this single place than all html files:
+for( const item of nav_items ){
+	if( item.innerText.match(/cases/i)){
+		add_cases( item )
+		break;
+	}
+}
+
+
+// rewrite links to environment
+for( const link of links ){
+	if( locale === 'dev' && link.href ){
+		const split = link.href.split('/')
+		link.href = env.APP_ROOT + split[ split.length -1 ]
+	}
+}
